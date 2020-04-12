8369081bd198f337f64310710c41f57e771de5ad
burusan123 <you@example.com>
2020-04-12 17:23:43 +0900

    update

--- _3DViewPython.py
+++ _3DViewPython.py
@@ -5,9 +5,9 @@ import math
 import _3D2D
 
 #世界座標 単位mm
-setting_z_mm = 3000
-setting_x_mm = 2000
-setting_y_mm = 2000
+setting_z_mm = 50000
+setting_x_mm = 500
+setting_y_mm = 1700
 
 #解像度設定
 width = 1920
@@ -18,8 +18,8 @@ center_y = height / 2
 #カメラ内部パラメータ
 fx = 648.04012214
 fy = 652.62477735
-cx = 0
-cy = 0
+cx = center_x
+cy = center_y
 
 #カメラ外部パラメータの回転行列で使用する角度
 x = 0
@@ -31,46 +31,42 @@ t1 = 0
 t2 = 0
 t3 = 0
 
+#カメラ歪みパラメータ
+k1 = -2.23845891e-01
+k2 = -7.32573873e-01
+p1 = -7.30350307e-04
+p2 = -1.68303803e-03
+k3 = 0.00000000e+00
+
 #描画する格子
-step = 30
-start_pos_x_mm = -setting_x_mm / 2
-start_pos_y_mm = -setting_y_mm / 2
-step_x = setting_x_mm / step
-step_y = setting_x_mm / step
+step = 10
+start_pos_x_mm = int(-setting_x_mm / 2)
+start_pos_y_mm = int(-setting_y_mm / 2)
+end_pos_x_mm = int(setting_x_mm / 2)
+end_pos_y_mm = int(setting_y_mm / 2)
+step_x = int(setting_x_mm / step)
+step_y = int(setting_x_mm / step)
+
 
 cv2.namedWindow("3D", cv2.WINDOW_AUTOSIZE)
 key = 0
-while (key != ord("q")):
-    rad_x = math.radians(x)
-    rad_y = math.radians(y)
-    rad_z = math.radians(z)
-    
+while (key != ord("q")):    
     img = np.zeros((height, width,3), dtype=np.uint8)
 
+    tranceform = _3D2D.Tranceform3D2D(fx,fy,cx,cy)
+    tranceform.SetExternalParameter(x, y, z, t1, t2, t3)
+    tranceform.SetDistortionParameter(k1, k2, k3, p1, p2)
+
     #　格子計算
-    y_mm = start_pos_y_mm
-    for pix_y in range(step):
-        x_mm = start_pos_x_mm
-        for pix_x in range(step):
-
+    for y_mm in range(start_pos_y_mm, end_pos_y_mm, step_y):
+        for x_mm in range(start_pos_x_mm, end_pos_x_mm, step_x):
             #　世界座標を画像座標に変換する
-            r = _3D2D.computeMatrixFromAngles(rad_x, rad_y, rad_z)
-            x_pix = fx * (r[0,0] * x_mm + r[0,1] * y_mm + r[0,2] * setting_z_mm + t1) / \
-                    (r[2,0]*x_mm + r[2,1] * y_mm + r[2,2] * setting_z_mm + t3) + cx
-            y_pix = fy * (r[1,0] * x_mm + r[1,1] * y_mm + r[1,2] * setting_z_mm + t2) / \
-                    (r[2,0]*x_mm + r[2,1] * y_mm + r[2,2] * setting_z_mm + t3) + cy
-
-            #　世界座標はカメラ中心が0,0の座標のため、座標変換する
-            x_pix = int(center_x + x_pix)
-            y_pix = int(center_y + y_pix)
+            x_pix, y_pix = tranceform.Cvt3Dto2D(x_mm, y_mm, setting_z_mm)
 
             if x_pix < width and 0 < x_pix and y_pix < height and y_pix > 0:
                 img[y_pix, x_pix, 0] = 255
                 img[y_pix, x_pix, 1] = 255
                 img[y_pix, x_pix, 2] = 255
-
-            x_mm += step_x
-        y_mm += step_y
 
     cv2.imshow("3D",img)
     key = cv2.waitKey(0) & 0xff
